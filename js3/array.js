13f63bb62827cb5537288dc39ebb444945390cc8
pingu073 <127610682+pingu073@users.noreply.github.com>
2023-03-11 08:42:19 -0600

    Add files via upload

---
+++ js3/array.js
@@ -0,0 +1,34 @@
+/* let cuenta=[5,7,3,4,7]
+document.write(cuenta[2])
+for(let i of cuenta){
+    document.write(i)
+} */
+/* let compras=[
+'casa','mesa'
+];
+compras[0]='tahini';
+console.log(compras) */
+/* let p1=prompt("primer palabra")
+let p2=prompt("segunda palabra")
+let p3=prompt("tercera palabra")
+let palabras=['perro','michi','pajaro','agua','coro','zoro','pala','ocho','pinocho','calidad']
+document.write(palabras)
+document.write("<br>")
+palabras[3]=p1
+palabras[5]=p2
+palabras[7]=p3
+document.write(palabras) */
+/* let p1=prompt("primer palabra")
+let p2=prompt("segunda palabra")
+let p3=prompt("tercera palabra")
+let p4=prompt("cuarta palabra")
+let p5=prompt("quinta palabra")
+let palabras=[p1,p2,p3,p4,p5]
+document.write(palabras)
+document.write("<br>")
+let pos=parseInt(prompt("quer posicion queres mostrar"))
+
+document.write(palabras[pos]) */
+let array=['dato','dato1']
+array.push('dato3')
+document.write(array)
---
+++ js3/cajita registradora.js
@@ -0,0 +1,17 @@
+//una caja inicialmentre cuenta con un saldo de 200,000 y se debe prosesar una venta de 20 mostrando al final al comprador el total a pagar y luego de recibir el pago mostrar el valor a devolver y saldo actualisado de la caja //
+//datos de entrada, productos, salod final, pago, cambio, costo de los productos//
+/* let pregun=parseInt(prompt("cuanto va a ser "));
+let cuanto=parseInt(prompt("le doy uno de"));
+let saldo=200000;
+let restar=(cuanto-pregun)
+let suma=(parseInt(cuanto)+saldo)
+document.write("<br>")
+document.write("total a pagar = "+pregun);
+document.write("<br>")
+document.write("pago del cliente = "+cuanto);
+document.write("<br>")
+document.write("su cambio es = " + restar);
+document.write("<br>")
+document.write("saldo de caja es = "+ suma);
+document.write("<br>")
+document.write("productos 20 ") */
